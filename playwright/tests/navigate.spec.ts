27f565a5e950646388362d7d5594a113c4e6948f
Kevin Toth <kevin.toth16@gmail.com>
2023-03-29 12:06:28 +0200

    Fix bug in playwright test

--- playwright/tests/navigate.spec.ts
+++ playwright/tests/navigate.spec.ts
@@ -3,9 +3,9 @@ import { test } from '@playwright/test';
 test('login test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByPlaceholder('Type here').click();
-  await page.getByPlaceholder('Type here').fill('Toby Emmanuel');
-  await page.getByPlaceholder('Password here').click();
-  await page.getByPlaceholder('Password here').fill('1234');
+  await page.getByPlaceholder('Please enter username').click();
+  await page.getByPlaceholder('Please enter username').fill('Toby Emmanuel');
+  await page.getByPlaceholder('Please enter password').click();
+  await page.getByPlaceholder('Please enter password').fill('1234');
   await page.getByRole('button', { name: 'Login' }).click();
 });
